686847accbd3fc4bc8b1695987882ef140fa6ce7
Deepak Goyal <dg887180@gmail.com>
2021-08-08 13:51:46 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var btnTranslate = document.querySelector("#btn");
+var outputText = document.querySelector("#output");
+var inputText = document.querySelector("textarea");
+var url = "https://api.funtranslations.com/translate/minion.json"
+
+function constructUrl(textInput) {
+    return url + "?" + "text=" + textInput;
+}
+function errorHandling(error){
+    alert("something went wrong on server,please try again after sometime")
+}
+function handleEvent() {
+    var textInput = inputText.value
+    fetch(constructUrl(textInput))
+        .then(response => response.json())
+        .then(data => { outputText.innerText = (data.contents.translated) 
+        })
+        .catch(errorHandling)
+}
+btn.addEventListener("click", handleEvent)
