115010646070e529a8b453228ce52852c8e541f9
Deepak Goyal <dg887180@gmail.com>
2021-08-08 14:27:02 +0530

    initial commit

--- app.js
+++ app.js
@@ -6,14 +6,15 @@ var url = "https://api.funtranslations.com/translate/minion.json"
 function constructUrl(textInput) {
     return url + "?" + "text=" + textInput;
 }
-function errorHandling(error){
+function errorHandling(error) {
     alert("something went wrong on server,please try again after sometime")
 }
 function handleEvent() {
     var textInput = inputText.value
     fetch(constructUrl(textInput))
         .then(response => response.json())
-        .then(data => { outputText.innerText = (data.contents.translated) 
+        .then(data => {
+            outputText.innerText = (data.contents.translated)
         })
         .catch(errorHandling)
 }
